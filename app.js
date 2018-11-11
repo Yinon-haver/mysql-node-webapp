6266462232b4de87f170634d76adf7fe6266973d
Yinon <yinon.hever@appsflyer.com>
2018-11-11 15:45:57 +0200

    add Redis

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 8080;
+var redis = require('redis');
 var mysql = require('mysql');
 var bodyParser = require('body-parser');
 
+
+
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-var connection = mysql.createConnection({
+
+var RedisClient = redis.createClient();
+RedisClient.on('error', function(err){
+    console.log(' RedisClient went wrong ', err)
+});
+
+var MySqlconnection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
     password : 'Qwert135!',
@@ -16,18 +25,29 @@ var connection = mysql.createConnection({
 
 app.get('/', function(req, res){
     var q = 'SELECT COUNT(*) AS count from users;'
-    connection.query(q, function (error, results, fields) {
+    MySqlconnection.query(q, function (error, results, fields) {
     if (error) throw error;
     var numberOfUsersInDB = results[0].count;
-    // res.send("We have "+ numberOfUsersInDB +" users in DB");
     res.render("home",{data:numberOfUsersInDB});
   });
 });
 
 
+app.post('/deleteUser', function (req, res) {
+    var email = req.body.deleteEmail;
+    var q = `DELETE FROM users WHERE email="${email}"`
+    MySqlconnection.query(q, function (error, results, fields) {
+        if (error) throw error;
+        console.log('deleted ' + results.affectedRows + ' rows');
+        res.redirect('/');
+    })
+
+})
+
 app.post('/register', function(req, res) {
     var person = {email: req.body.email}
-    connection.query('INSERT INTO users SET ?', person, function (error, results, fields) {
+    RedisClient.set('yinon', req.body.email);
+    MySqlconnection.query('INSERT INTO users SET ?', person, function (error, results, fields) {
         if (error) throw error;
         console.log(results);
         res.redirect('/');
@@ -35,4 +55,4 @@ app.post('/register', function(req, res) {
 });
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`MySql app listening on port ${port}!`))
